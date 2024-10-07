1f4faf197f45d5a1a2f8b2c02df380ba49203792
RimuruChan <RealSprite233@outlook.com>
2024-10-07 14:47:52 +0800

    fix: fix for vscode 1.94.0

--- extension.js
+++ extension.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const path = require('path')
 const crypto = require('crypto')
 
-const appDir = path.dirname(require.main.filename)
+const appDir = path.join(path.dirname(process.execPath), "resources/app/out")
 const rootDir = path.join(appDir, '..')
 
 const productFile = path.join(rootDir, 'product.json')
